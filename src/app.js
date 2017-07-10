655c2bd3fa18c72b020e5d57549ac8527bb1a516
bkucera3 <benjamin.kucera.stu@gtri.gatech.edu>
2017-07-10 12:04:01 -0400

    pages resources

--- src/app.js
+++ src/app.js
@@ -5,26 +5,26 @@ var animation = bodymovin.loadAnimation({
   renderer: 'svg',
   loop: true,
   autoplay: true,
-  path: '/resources/mild_wave.json'
+  path: './resources/mild_wave.json'
 })
 var animation2 = bodymovin.loadAnimation({
   container: document.getElementById('bm2'),
   renderer: 'svg',
   loop: true,
   autoplay: true,
-  path: '/resources/check_mark.json'
+  path: './resources/check_mark.json'
 })
 var animation3 = bodymovin.loadAnimation({
   container: document.getElementById('bm3'),
   renderer: 'svg',
   loop: true,
   autoplay: true,
-  path: '/resources/changing.json'
+  path: './resources/changing.json'
 })
 var animation4 = bodymovin.loadAnimation({
   container: document.getElementById('bm4'),
   renderer: 'svg',
   loop: true,
   autoplay: true,
-  path: '/resources/jump_loader.json'
+  path: './resources/jump_loader.json'
 })
